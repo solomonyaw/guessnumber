aa104134a64d62eac057c094845b7867e6df27a9
Raymond92-hub <sadeklo@st.vvu.edu.gh>
2022-08-30 09:06:23 -0700

    added all files

---
+++ script.js
@@ -0,0 +1,8 @@
+'use strict';
+
+// document.querySelector('.message').textContent = 'Correct Number';
+// document.querySelector('.number').textContent=13;
+// document.querySelector('.score').textContent = 10;
+// document.querySelector('.guess').value = 10;
+// console.log(document.querySelector('.guess').value);
+document.querySelector('.check').addEventListener('click');
