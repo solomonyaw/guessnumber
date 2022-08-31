0c92f31e251e04de8f8765d942e558fcb6a139aa
Raymond92-hub <sadeklo@st.vvu.edu.gh>
2022-08-30 18:21:50 -0700

    updated html and javascript files

--- script.js
+++ script.js
@@ -5,4 +5,77 @@
 // document.querySelector('.score').textContent = 10;
 // document.querySelector('.guess').value = 10;
 // console.log(document.querySelector('.guess').value);
-document.querySelector('.check').addEventListener('click');
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+//display message function
+    function displayMessage(message)
+    {
+        document.querySelector('.message').textContent = message;
+    }
+
+
+document.querySelector('.check').addEventListener('click', function () {
+    const guess = Number(document.querySelector('.guess').value);
+    
+  //When there is no input
+  if (!guess) {
+      displayMessage('No number! ⛔');
+  }
+   
+  //When player wins
+  else if (guess === secretNumber) {
+      displayMessage('Correct Number! 🥳');
+    document.querySelector('.output').textContent = 'You won! 🥳';
+    document.querySelector('.number').textContent = secretNumber;
+    document.querySelector('body').style.backgroundColor = '#60b347';
+      document.querySelector('.number').style.width = '30rem';
+      
+      // When guess is wrong
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+        displayMessage(guess > secretNumber ? 'Too high! 📈' : 'Too Low! 📉');
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+        displayMessage('You lost the game!');
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+
+/*   //When guess is too high
+  else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too high! 📈';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+
+  //When guess is too low
+  else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too Low! 📉';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
+  } */
+ });
+
+//Reset Game
+document.querySelector('.again').addEventListener('click', function () {
+ displayMessage('Start guessing');
+  document.querySelector('body').style.backgroundColor = '#222';
+  score = 20;
+  document.querySelector('.score').textContent = 20;
+  document.querySelector('.output').textContent = 'Guess My Number!';
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+   secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('.number').style.width = '10rem';
+});
